c5a351d1294242b08a423fc30569551d4f885041
Maypher92 <eva.matus@hotmail.com>
2025-06-28 14:01:12 -0400

    Form fields and basic styles

--- script.js
+++ script.js
@@ -4,3 +4,21 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 
+const elementos = {
+  formulario: null,
+  nombre: null,
+  precio: null,
+  categoria: null,
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Probando Etapa 2");
+  obtenerElementosDOM();
+});
+
+function obtenerElementosDOM() {
+  elementos.formulario = document.getElementById("formulario-producto");
+  elementos.nombre = document.getElementById("nombre");
+  elementos.precio = document.getElementById("precio");
+  elementos.categoria = document.getElementById("categoria");
+}
